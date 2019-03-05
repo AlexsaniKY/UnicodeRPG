a294d79d545c14ecff8acb3cf64c4f08d3803579
AlexsaniKY <alexsaniwilliams@gmail.com>
2019-03-05 12:18:04 -0500

    added dither and clamp functions

--- src/shared/numbers.ts
+++ src/shared/numbers.ts
@@ -1,5 +1,5 @@
 export function randInt(min:number, max:number): number {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export function randFloat(min:number, max:number): number {
@@ -12,4 +12,32 @@ export function randBool(): boolean {
 
 export function wrap(value: number, mod: number) {
     return ((value % mod) + mod) % mod;
+}
+
+export function clamp(value:number, min:number, max:number) {
+    return Math.min(Math.max(value, min), max);
+  };
+
+export class Dither{
+    gen: IterableIterator<number>;
+    constructor(){
+        this.gen = ditherGen();
+        this.gen.next();
+    }
+    
+    next(val: number){
+        return this.gen.next(val).value;
+    }
+}
+
+function* ditherGen(){
+    let error:number = 0;
+    let value:number = 0;
+    let int_val:number = 0;
+    while(true){
+      value = yield int_val;
+      int_val = Math.floor(value + error);
+      error += value - int_val;
+      //console.log("error", error);
+    }
 }
